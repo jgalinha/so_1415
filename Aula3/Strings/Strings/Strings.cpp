5495ba94be75521628ed6edf41596ae4e9acdfb0
José Eduardo Beiçudo Galinha <j.b.galinha@gmail.com>
2014-10-02 18:54:13 +0100

    Série de Exercicios - Ex1

--- Aula3/Strings/Strings/Strings.cpp
+++ Aula3/Strings/Strings/Strings.cpp
@@ -1,16 +1,6 @@
 // Strings.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-
-#define MAX_PAL 100
-
-
 #include "stdafx.h"      /*ficheiro com definições do Windows*/
 
 #include <stdio.h>
@@ -20,23 +10,26 @@
 
 #define MAX_PAL 20
 
+/*Série de Exercicios 1 - Ex1*/
+
 int main(){
 	char palavra1[MAX_PAL];
-	char palavra2[MAX_PAL];
+	int i;
 
 	setlocale(LC_ALL, "Portuguese");
 
 	printf("Introduza uma palavra:\n");
 	scanf("%s", palavra1);
-	printf("Introduza outra palavra:\n");
-	scanf("%s", palavra2);
 
-	if (strcmp(palavra1, palavra2) == 0)
-		printf("São iguais!\n");
-	else
-		printf("São diferentes!\n");
+	i = 0;
+	while (palavra1[i] != '\0')
+	{
+		i++;
+	}
+
+	printf("A palavra tem %d carateres\n", i);
+
 
 	system("pause");
-
 	return 0;
 }

dc54479a1b44ad9b74e2be450e46e36a02c2a3ec
José Eduardo Beiçudo Galinha <j.b.galinha@gmail.com>
2014-10-02 17:56:22 +0100

    Ex1 - Aula3

--- Aula3/Strings/Strings/Strings.cpp
+++ Aula3/Strings/Strings/Strings.cpp
@@ -8,11 +8,23 @@
 #include <stdlib.h>
 #include <locale.h>
 
-#define MAX_PAL 20
+#define MAX_PAL 100
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
+
+	setlocale(LC_ALL, "Portuguese");
+
+	char david[MAX_PAL];
+	char david2[MAX_PAL];
+	strcpy(david, "vidigueira larga o osso,");
+	strcpy(david2, "está na hora do almoço!");
+
+	printf("%s %s \n", david, david2);
+	system("pause");
+
+
+
 	return 0;
 }
-

b3d4c5292e453475500951cb0b06cf97599d614b
José Eduardo Beiçudo Galinha <j.b.galinha@gmail.com>
2014-10-02 17:32:57 +0100

    Exercicios Aula 2

---
+++ Aula2/prog1/prog1/prog1.cpp
@@ -0,0 +1,18 @@
+// prog1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Portuguse");
+	printf("Pe*** !\n");
+	
+	system("pause");
+
+	return 0;
+}
+
---
+++ Aula3/Strings/Strings/Strings.cpp
@@ -0,0 +1,18 @@
+// Strings.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define MAX_PAL 20
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
